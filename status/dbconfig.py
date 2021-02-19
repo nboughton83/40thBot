cabdf3eef935eeb5c78b7ca32af2c731de034e5c
nboughton83 <nboughton+github@pm.me>
2021-02-19 01:21:33 -0500

    first commit

---
+++ status/dbconfig.py
@@ -0,0 +1,23 @@
+DB_HOST = "192.168.13.6"
+DB_USERNAME = "perun" 
+DB_PASSWORD = "dcsded"
+DB_DATABASE = "perun"
+
+servers = {
+    1: {
+        "serverFullname": "40thsoc.org - Mission Server",
+        "alias":"Mission"
+        },
+    2: {
+        "serverFullname": "40thsoc.org - Training Server",
+        "alias":"Training"
+        },
+    3: {
+        "serverFullname": "40thsoc.org - Dynamic Server",
+        "alias":"Dynamic"
+        },
+    4: {
+        "serverFullname": "40thsoc.org - Weekly Mission Server",
+        "alias":"Weekly"
+        },
+        }
